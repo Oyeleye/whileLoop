819f48be307fbb33328b63b7091392214f756c9f
Oyeleye <oyeleyeoyedayo@gmail.com>
2020-01-14 23:32:34 +0200

    first commit

---
+++ whileloops.js
@@ -0,0 +1,41 @@
+//print all numbers between -10 and 19
+var allNumbers= -10;
+while (allNumbers<=19) {
+  console.log("They are " + allNumbers);
+  allNumbers++;
+}
+
+//print all even numbers between 10 and 40
+var evenNumbers=10;
+while(evenNumbers<=40) {
+	console.log("Even number: " + evenNumbers);
+	evenNumbers+=2;
+}
+
+//or   //print all even numbers between 10 and 40
+//var evenNumbers=10;
+//while(evenNumbers<=40){ 
+//  if(evenNumbers%2===0){   
+//	console.log("Even number:" + evenNumbers);
+//    }
+//	evenNumbers++;
+//}
+
+//print all odd numbers between 300 and 333
+var oddNumbers=300;
+while(oddNumbers<=333===0) {
+	console.log("Odd number:" + oddNumbers);
+    }
+	oddNumbers++;
+}
+
+
+//print all numbers divisible by 5 and 3, between 5 and 50
+var numbers=5;
+while(numbers<51) {
+	if (numbers%5===0 && numbers%3===0){
+	console.log("Numbers:" + numbers);
+    }
+	numbers++;
+}
+
